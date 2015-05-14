ff3a8dc531575bd4604980161f77bc4699e351a1
marc <marc@debian.marc.td.teradata.com>
2015-05-14 13:33:56 -0500

    Fixed the timestamps to use seconds and not milliseconds.

--- DataGenerator.java
+++ DataGenerator.java
@@ -1946,8 +1946,6 @@ public class DataGenerator {
       com.github.javafaker.Address a = faker.address();
       String username = person.username(),
              password = person.password(),
-             created = new Date(uRandomCreated()).toLocaleString(),
-             lastLogin = new Date(uRandomLogin()).toLocaleString(),
              status = "active",
              firstname = person.firstName(),
              lastname = person.lastName(),
@@ -1961,6 +1959,8 @@ public class DataGenerator {
              phonenumber1 = person.telephoneNumber(),
              phonenumber2 = "NULL",
              email = person.email();
+      Double created = Math.floor(uRandomCreated() / 1000.0D),
+             lastLogin = Math.floor(uRandomLogin() / 1000.0D);
 
       JsonObject o = new JsonObject();
       o.addProperty("id", id);
@@ -2098,7 +2098,7 @@ public class DataGenerator {
         e = format.parse("2015-07-04 23:59:59");
       }
 
-      writerMC.write("INSERT INTO MyECommerce.tdMarketingCampaign VALUES(" + n + ",'Loyal Customers','" + removeLastChar(StringUtils.join(tp.paragraph(5), ",")) + "',1,cast(cast(700101 as date) + " + s.getTime() + " / 86400 as timestamp(6)) + (" + s.getTime() + " mod 86400) * interval '00:00:01' hour to second" + ",cast(cast(700101 as date) + " + e.getTime() + " / 86400 as timestamp(6)) + (" + e.getTime() + " mod 86400) * interval '00:00:01' hour to second);\n");
+      writerMC.write("INSERT INTO MyECommerce.tdMarketingCampaign VALUES(" + n + ",'Loyal Customers','" + removeLastChar(StringUtils.join(tp.paragraph(5), ",")) + "',1,cast(cast(700101 as date) + " + (s.getTime() / 1000) + " / 86400 as timestamp(6)) + (" + (s.getTime() / 1000) + " mod 86400) * interval '00:00:01' hour to second" + ",cast(cast(700101 as date) + " + (e.getTime() / 1000) + " / 86400 as timestamp(6)) + (" + (e.getTime() / 1000) + " mod 86400) * interval '00:00:01' hour to second);\n");
       writerD.write("INSERT INTO MyECommerce.tdDiscount VALUES(" + n + "," + n + ",'" + iterator.next() + "', 0.4);\n");
       n++;
     }
@@ -2212,8 +2212,8 @@ public class DataGenerator {
         (0.32455 * weight) + "," +
         tax + "," +
         weight + ",cast(cast(700101 as date) + " + 
-        created + " / 86400 as timestamp(6)) + (" + created + " mod 86400) * interval '00:00:01' hour to second" + ",cast(cast(700101 as date) + " + 
-        created + " / 86400 as timestamp(6)) + (" + created + " mod 86400) * interval '00:00:01' hour to second," +
+        (created / 1000) + " / 86400 as timestamp(6)) + (" + (created / 1000) + " mod 86400) * interval '00:00:01' hour to second" + ",cast(cast(700101 as date) + " + 
+        (created / 1000) + " / 86400 as timestamp(6)) + (" + (created / 1000) + " mod 86400) * interval '00:00:01' hour to second," +
         (status.equals("completed") ? 1 : 0) + ",'" +
         status + "',NULL,'" +
         customers.get(customer - 1).getAsJsonObject().get("firstname").getAsString() + "','" +

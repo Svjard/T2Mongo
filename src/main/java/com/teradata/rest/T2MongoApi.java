3bb521870bb152833ccedfdccb490ad2b0fa9798
marc <marc@debian.marc.td.teradata.com>
2015-05-20 18:04:47 -0500

    Added CORS support for query1.

--- src/main/java/com/teradata/rest/T2MongoApi.java
+++ src/main/java/com/teradata/rest/T2MongoApi.java
@@ -110,7 +110,7 @@ public class T2MongoApi {
       }
     }
 
-    rp = Response.ok(data).build();
+    rp = Response.ok(data)..header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS").build();
     return rp;
   }
 

42c68c9d7a440ade48335b6eb58fac1a88cf8f8c
marc <marc@debian.marc.td.teradata.com>
2015-05-21 10:25:48 -0500

    Another syntax fix to the rest api.

--- src/main/java/com/teradata/rest/T2MongoApi.java
+++ src/main/java/com/teradata/rest/T2MongoApi.java
@@ -341,7 +341,7 @@ public class T2MongoApi {
     try {
       s = connection.createStatement();
 
-      String query += 
+      String query = 
         "SELECT b.\"name\", b.description, CAST(CAST(b.\"start\" AS DATE FORMAT 'YYYY-MM-DD') AS VARCHAR(50)), CAST(CAST(b.\"end\" AS DATE FORMAT 'YYYY-MM-DD') AS VARCHAR(50)), a.code, a.amount " +
         "FROM \"MyECommerce\".\"tdDiscount\" a " +
         "JOIN \"MyECommerce\".\"tdMarketingCampaign\" b " +
